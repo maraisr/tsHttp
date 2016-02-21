21f43033845398f99604c6e16416223d0817ac27
Marais Rossouw <me@maraisr.com>
2016-02-22 10:18:33 +1100

    VS Code init and other small changes

--- src/HTTP.ts
+++ src/HTTP.ts
@@ -1,19 +1,21 @@
 export module HTTP {
-    var base:string,
-		_config:ClientConfig;
+    var base: string,
+		_config: ClientConfig;
+
+	export const GET = 'GET';
 
 	interface ClientConfig {
-		params?:Object;
+		params?: Object;
 	}
 
     export class Client {
-        constructor(nBase:string, config?:ClientConfig) {
+        constructor(nBase: string, config?: ClientConfig) {
             base = nBase;
 			_config = config;
         }
 
-        public get(ep:string, params:Object = void {}):Promise<Response> {
-            return (new Request('GET', {base: base, ep: ep, params: params})).send();
+        public get(ep: string, params: Object = void {}): Promise<Response> {
+            return (new Request(HTTP.GET, { base: base, ep: ep, params: params })).send();
         }
     }
 
@@ -24,11 +26,11 @@ export module HTTP {
     }
 
     export class Request {
-        private r:XMLHttpRequest;
-        private url:RequestConfig;
-        method:string;
+        private r: XMLHttpRequest;
+        private url: RequestConfig;
+        method: string;
 
-        constructor(method:string, url:RequestConfig) {
+        constructor(method: string, url: RequestConfig) {
             this.method = method;
             this.url = url;
 
@@ -39,12 +41,12 @@ export module HTTP {
             this.r = new XMLHttpRequest();
         }
 
-        static enParam(params:Object, prefix:string = ''):string {
-            var r:Array<any> = new Array();
+        static enParam(params: Object, prefix: string = ''): string {
+            var r: Array<any> = new Array();
 
             for (var p in params) {
-                var k:string = ((prefix != '') ? (prefix + '[' + p + ']') : p),
-                    v:any = params[p];
+                var k: string = ((prefix != '') ? (prefix + '[' + p + ']') : p),
+                    v: any = params[p];
 
                 if (!(v == null || typeof v == void 0)) {
                     r.push((typeof v == 'object') ? Request.enParam(v, k) : encodeURIComponent(k) + '=' + encodeURIComponent(v));
@@ -54,11 +56,11 @@ export module HTTP {
             return r.join('&');
         }
 
-        static buildQuery(base:string, ep:string, params:Object):string {
+        static buildQuery(base: string, ep: string, params: Object): string {
             return base.replace(/\/+$/, '') + '/' + ep.replace(/^\/+/, '') + (params == void {} ? '' : '?' + Request.enParam(params));
         }
 
-        send():Promise<Response> {
+        send(): Promise<Response> {
             return new Promise((resolve, reject) => {
                 this.r.open(this.method, Request.buildQuery(this.url.base, this.url.ep, this.url.params), true);
                 this.r.setRequestHeader('Accept', 'application/json');
@@ -74,12 +76,12 @@ export module HTTP {
     }
 
     class Response {
-        private response:any;
-        private isJson:boolean = false;
-        json:Object = void 0;
-        statusCode:number;
+        private response: any;
+        private isJson: boolean = false;
+        json: Object = void 0;
+        statusCode: number;
 
-        constructor(r:XMLHttpRequest) {
+        constructor(r: XMLHttpRequest) {
             this.response = r.responseText;
 
             this.statusCode = r.status;
@@ -93,17 +95,17 @@ export module HTTP {
     }
 
     export class Pool {
-        client:Client;
-        requests:Array<Request>;
+        client: Client;
+        requests: Array<Request>;
 
-        constructor(client:Client, requests:Array<Request>) {
+        constructor(client: Client, requests: Array<Request>) {
             this.client = client;
             this.requests = requests;
         }
 
-        send(cb:(response:Response, index:number) =>  any):void {
-            for (let i:number = 0; i < this.requests.length; i++) {
-                this.requests[i].send().then((response:Response) => {
+        send(cb: (response: Response, index: number) => any): void {
+            for (let i: number = 0; i < this.requests.length; i++) {
+                this.requests[i].send().then((response: Response) => {
                     cb(response, i);
                 });
             }
